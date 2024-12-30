007c5305345c1e5114afe198f32ba03a2087a125
Abderrazek SAIDANE <abdsaidane95@gmail.com>
2024-12-30 17:44:32 +0100

    screen bleu

--- public/js/csv_to_html_table.js
+++ public/js/csv_to_html_table.js
@@ -68,47 +68,6 @@ CsvToHtmlTable = {
         $table.DataTable(datatables_options);
     },
 
-        // Chargement et traitement du fichier CSV
-            /*       
-            $.when($.get(csv_path)).then(
-            function (data) {
-
-                var csvData = $.csv.toArrays(data, csv_options);
-                // Création du header de la table
-                var $tableHead = $("<thead></thead>");
-                var csvHeaderRow = csvData[0];
-                var $tableHeadRow = $("<tr></tr>");
-                for (var headerIdx = 0; headerIdx < csvHeaderRow.length; headerIdx++) {
-                    $tableHeadRow.append($("<th></th>").text(csvHeaderRow[headerIdx]));
-                }
-                $tableHead.append($tableHeadRow);
-                $table.append($tableHead);*/
-                
-                // Création du corps de la table
-                /*var $tableBody = $("<tbody></tbody>");
-                for (var rowIdx = 1; rowIdx < csvData.length; rowIdx++) {
-                    var $tableBodyRow = $("<tr></tr>");
-                    for (var colIdx = 0; colIdx < csvData[rowIdx].length; colIdx++) {
-                        var $tableBodyRowTd = $("<td data-label='"+csvData[0][colIdx]+"'></td>");
-
-                        // Utilisation de templates personnalisés si disponibles
-                        var cellTemplateFunc = customTemplates[colIdx];
-                        if (cellTemplateFunc) {
-                            $tableBodyRowTd.append($("<td id='Drapeau'></td>")).html(cellTemplateFunc("../css/images/"+csvData[rowIdx][colIdx+1]+".jpg", rowIdx, colIdx));
-                        } else {
-                            $tableBodyRowTd.text(csvData[rowIdx][colIdx]);
-                        }
-                        $tableBodyRow.append($tableBodyRowTd);
-                    }
-                    $tableBody.append($tableBodyRow);
-                }
-                $table.append($tableBody);
-
-                // Initialisation de DataTables
-                $table.DataTable(datatables_options);
-            });
-    },*/
-
     // Fonction pour afficher des images si la cellule contient une URL
     imageFormatter: function (cellValue) {
         if (cellValue.split(";")[0]) {
@@ -116,7 +75,7 @@ CsvToHtmlTable = {
             img.attr("src", cellValue);
             img.attr("alt", "Image");
             img.css({
-                width: "50px",
+                width: "80px",
                 height: "auto",
                 borderRadius: "5px",
             });
@@ -127,19 +86,3 @@ CsvToHtmlTable = {
     
 
 };
-
-// Exemple d'utilisation
-/*CsvToHtmlTable.init({
-    csv_path: "/data/test2.csv",
-    element: "table-container",
-    custom_formatting: [
-        [0, CsvToHtmlTable.imageFormatter] // Applique l'affichage des images à la première colonne
-    ],
-    datatables_options: {
-        paging: true,
-        fixedHeader: true, // Pour un en-tête fixe
-        scrollX: true, // Scrolling horizontal
-    }
-     // l'affichage des drapeaux ne fonctionne pas
-      // https://datatables.net/extensions/fixedheader/examples/fixed_columns.html
-});*/
